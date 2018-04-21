b40709af0158908e6d50d6c8d627beb65d98e915
Eduardo Luz <eduluz1976@gmail.com>
2018-04-21 15:41:48 +0000

    Adding documentation

--- src/ufw/exception/AppException.php
+++ src/ufw/exception/AppException.php
@@ -7,6 +7,13 @@ class AppException extends \Exception {
     
     protected $httpCode=false;
     
+    /**
+     * 
+     * @param string $message
+     * @param int $code
+     * @param int $httpCode
+     * @param \Throwable $previous
+     */
     public function __construct(string $message = "", int $code = 0, $httpCode=false, \Throwable $previous = null) {
         parent::__construct($message, $code, $previous);
         if ($httpCode) {
@@ -14,7 +21,10 @@ class AppException extends \Exception {
         }
     }
     
-    
+    /**
+     * 
+     * @return int
+     */
     public function getHttpCode() {
         return $this->httpCode;
     }
