2325fc7d582f31ba77c0ccd95094fafebc437ea5
Eduardo Luz <eduluz1976@gmail.com>
2018-05-29 06:29:51 +0000

    Fixing autoload issue

--- src/composer_autoloader.php
+++ src/composer_autoloader.php
@@ -8,13 +8,14 @@ return function () : bool {
       __DIR__ . '/../../../autoload.php',  // composer dependency
       __DIR__ . '/../vendor/autoload.php', // stand-alone package
     );
-    $response = true;
+    $response = false;
     foreach ($files as $file) {
         if (is_file($file)) {            
             require_once $file;
+            $response = true;
             //return true;
-        } else {
-            $response = false;
+//        } else {
+//            $response = false;
         }
     }
     return $response;
